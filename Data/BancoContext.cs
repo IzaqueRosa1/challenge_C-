af8a0568af20ed38e86faa92abe3527b7ada4ec9
Izaque Rosa <izaquerosadesouza@gmail.com>
2023-01-28 14:34:51 -0300

    first commit

---
+++ Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using DesafioEstagio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioEstagio.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
